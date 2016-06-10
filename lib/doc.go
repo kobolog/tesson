412f8ebbfab3492881b1669801ce70089561ec22
Andrey Sibiryov <kobolog@uber.com>
2016-06-09 23:27:20 -0400

    [core] amended file headers.

--- lib/doc.go
+++ lib/doc.go
@@ -1,11 +1,26 @@
+// Copyright (c) 2016 Andrey Sibiryov <me@kobology.ru>
+// Copyright (c) 2016 Other contributors as noted in the AUTHORS file.
+//
+// This file is part of Tesson.
+//
+// Tesson is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// Tesson is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 // Package tesson contains Tesson core implementation details.
 //
-// It has three main abstractions:
-// - Topology
-// - RuntimeContext
-// - Frontend
+// Tesson has several main abstractions: Topology, RuntimeContext and Frontend.
 //
-// Topology is responsible for gathering the information about hardware layout,
+// Topology's responsible for gathering the information about hardware layout,
 // analysing it and generating a deployment plan.
 //
 // RuntimeContext is an abstraction over execution engine, e.g. Docker Engine.
